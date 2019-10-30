14b485b38f850774f0b7131890961c8db511b51e
shyjuonetikk <shyju@onetikk.com>
2019-10-30 15:48:40 +0530

    fb login removed

--- src/Controller/UsersController.php
+++ src/Controller/UsersController.php
@@ -4,7 +4,20 @@
 
    class UsersController extends AppController{
       public function login(){
-      	 
-	      }
+      	
+      		$fb = new Facebook\Facebook([
+  'app_id' => '510749116427614',
+  'app_secret' => '308009010bfa314e2ce22bef5516a86b',
+  'default_graph_version' => 'v2.10',
+  ]);
+
+$helper = $fb->getRedirectLoginHelper();
+
+$permissions = ['email']; // Optional permissions
+$loginUrl = $helper->getLoginUrl('https://shyjufacebooklogin.herokuapp.com/fb-callback.php', $permissions);
+
+echo '<a href="' . $loginUrl . '">Log in with Facebook!</a>'; 
+	    
+	    }
    }
 ?>
